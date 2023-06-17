6182060baaf0ed14f3ff7f5141c3c67d68307532
cha777 <chathuranga_wm@yahoo.com>
2023-06-17 11:16:22 +0530

    html content delivery

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 const express = require('express');
 
+const fs = require('fs');
 const path = require('path');
 
 const configPath = path.resolve(__dirname, 'firebase-config.json');
@@ -17,15 +18,6 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const payload = {
-  notification: {
-    title: 'FCM IS COOL !',
-    body: 'Notification has been recieved',
-    content_available: 'true',
-    image: 'https://i.ytimg.com/vi/iosNuIdQoy8/maxresdefault.jpg',
-  },
-};
-
 const options = {
   priority: 'high',
 };
@@ -38,8 +30,15 @@ app.get('/', (req, res) => {
   res.send('express service running');
 });
 
-app.get('/notification/sendToSpecific', (_, res) => {
+app.get('/notification/send-basic', (_, res) => {
   console.log('tokens', tokens);
+
+  const payload = {
+    notification: {
+      title: 'Pro11 Plain Notification',
+      body: 'You have got a basic notification',
+    }
+  }
 
   admin
     .messaging()
@@ -51,6 +50,33 @@ app.get('/notification/sendToSpecific', (_, res) => {
       res.status(500).send(error);
     });
 });
+
+app.get('/notification/send-advanced', async (_, res) => {
+  console.log('tokens', tokens);
+
+  const htmlContentPath = path.resolve(__dirname, '../html-notification/index.html');
+  const htmlContent = await fs.promises.readFile(htmlContentPath, {encoding: 'utf-8'});
+
+  const payload = {
+    notification: {
+      title: 'Pro11 HTML Notification',
+      body: 'You have got a html notification',
+    },
+    data: {
+      additionalContent: htmlContent
+    }
+  }
+
+  admin
+      .messaging()
+      .sendToDevice(tokens, payload, options)
+      .then(() => {
+        res.status(200).send('message successfully sent !');
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+})
 
 app.post('/notification/register', (req, res) => {
   const { token, pdm } = req.body;
